78f4518ca85e0aa0f974261087443165abfa4502
Tintin V <tintin.vocard@>
2024-12-30 20:47:21 +0100

    CI/CD : ajouter configuration AWS codeDeploy

---
+++ scripts/ec2/installJava.sh
@@ -0,0 +1,14 @@
+
+# installation sur le serveur ec2 :
+## installer java 17
+sudo yum install java-17-amazon-corretto
+
+## installer maven
+wget http://mirror.olnevhost.net/pub/apache/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+tar xvf apache-maven-3.9.9-bin.tar.gz
+sudo mkdir /usr/local/apache-maven
+sudo mv apache-maven-3.9.9 /usr/local/apache-maven/
+rm apache-maven-3.9.9-bin.tar.gz
+
+## globaliser la commande $mvn
+sudo ln -sf /usr/local/apache-maven/apache-maven-3.9.9/bin/mvn /usr/bin/mvn
